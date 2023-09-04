c3c1a4e88b93e4180b45d4581ac8a98c5c8db878
cole <c>
2023-09-04 11:12:19 -0500

    errors galore...too many issues and i could just go retrieve everything as i need it

--- FetchMeFoss/Models/SoftwareConfigInfo.cs
+++ FetchMeFoss/Models/SoftwareConfigInfo.cs
@@ -4,20 +4,23 @@
     public struct SoftwareConfigInfo
     {
         // todo 4; extend and add a fetchable description?
-        public string AppTitle { get; set; }
-        public string FileType { get; set; }
-        public string VersionNo { get; set; }
         // todo 4; SiteDownloadPageLink can possibly grow to a list one day if warranted
         // todo 4; Rename SiteDownloadPageLink to just DownloadFromHtmlParsingLink
-        public string SiteDownloadPageLink { get; set; }
-        public string BaseUri { get; set; } // todo 4; break up BaseUri to be "Scheme" and "HostName"
-        public string UriPathToExec { get; set; }
-        public string FileName { get; set; }
+        // todo 4; add a "notes" or "info" section, not description. hints, errors, issues, etc.
         // todo 4; add a "previous version"
         // todo 4; add a "last date pulled" for version info
         // todo 4; add 32bit, 64bit, arm version search so that if first is 32, skip
         // todo 4; rename FullLink to be "DownloadDirectlyFullLink
         // todo 4; need speciality links for unique pages like chromium where link redirects
+        // todo 4; break up BaseUri to be "Scheme" and "HostName"
+        // todo 4; add a "True/False" section for "Do you want to download?" since not all of them will be needed
+        public string AppTitle { get; set; }
+        public string FileType { get; set; }
+        public string VersionNo { get; set; }
+        public string SiteDownloadPageLink { get; set; }
+        public string BaseUri { get; set; }
+        public string UriPathToExec { get; set; }
+        public string FileName { get; set; }
         public string FullLink 
         { 
             get 
@@ -25,6 +28,5 @@
                 return BaseUri + UriPathToExec + FileName + "." + FileType; 
             } 
         }
-        // Used for pattern matching and switching out if all other methods fail/is faster
     }
 }
